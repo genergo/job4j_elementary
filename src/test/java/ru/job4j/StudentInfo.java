02078358d28dc40edeb0b59b4620fa605ed6e764
genergo <genergo.ufa@gmail.com>
2022-03-01 20:03:36 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/test/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+            System.out.print("Вася Иванов!!!");
+            System.out.println("29.06.1983");
+            System.out.print("381*1");
+    }
+}
