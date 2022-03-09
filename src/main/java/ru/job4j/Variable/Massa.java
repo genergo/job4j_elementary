36b9625b946f4967873eed4b26f4d712396aac33
genergo <genergo.ufa@gmail.com>
2022-03-09 17:33:30 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Variable/Massa.java
@@ -0,0 +1,25 @@
+package ru.job4j.Variable;
+
+public class Massa {
+        public static double manWeight(short height) {
+            double rsl = (height - 100) * 1.15;
+            return rsl;
+
+        }
+
+        public static double femanWeight(short height) {
+            double rsl = (height - 110) * 1.15;
+            return rsl;
+                }
+
+        public static void main(String[] args) {
+            short manheight = 187;
+            double man = Massa.manWeight(manheight);
+            System.out.println("Мужчина при росте 187 см может иметь идеальный вес " + man + " кг ");
+            short femanheight = 180;
+            double feman = Massa.femanWeight(femanheight);
+            System.out.println("Женщина при росте 180 см может иметь идеальный вес " + feman + " кг ");
+
+        }
+    }
+
---
+++ src/main/java/ru/job4j/Variable/WhatIsYourName.java
@@ -0,0 +1,12 @@
+package ru.job4j.Variable;
+
+import javax.swing.*;
+
+    public class WhatIsYourName {
+
+        public static void main(String[] args) {
+            String name;
+            name = JOptionPane.showInputDialog("Добрый день! Ваш вес?");
+            JOptionPane.showMessageDialog(null, "Принято \n " + name + "!");
+        }
+}
