865f9e34f5dd16eb6bf0b8946c8e433e2687d154
genergo <genergo.ufa@gmail.com>
2022-03-10 09:35:46 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Info/condition/Point.java
@@ -0,0 +1,23 @@
+package ru.job4j.Info.condition;
+import java.lang.Math;
+public class Point {
+    public static double minus(int x1, int y1, int x2, int y2) {
+        double rsl1 = x2 - x1;
+        double rsl2 = y2 - y1;
+        System.out.println(rsl1);
+        System.out.println(rsl2);
+    }
+
+    public static double pow1(double rsl1, int 2) {
+        return StrictMath.pow(rsl1, 2);
+    }
+
+    public static double pow2(double rsl2, double 2) {
+        return StrictMath.pow(rsl2, 2);
+    }
+
+
+    public static double plus(int rsl1, int rsl2) {
+        double rsl3 = rsl1 + rsl2;
+        System.out.println(rsl3);
+    }
