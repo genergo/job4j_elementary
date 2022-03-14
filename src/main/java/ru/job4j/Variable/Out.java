f6647c1b44f97731df138c720c6b55c46a7d0ae8
genergo <genergo.ufa@gmail.com>
2022-03-14 10:10:32 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Variable/Out.java
@@ -0,0 +1,14 @@
+package ru.job4j.Variable;
+
+public class Out {
+    public static void main(String[] args) {
+        String lyrics = "Мама мыла раму";
+        System.out.println(lyrics);
+        String name = "Petr";
+        byte age = 33;
+        String surname = ("Ivanov, " + age + " года ");
+        String separate = " - ";
+        String id = name + separate + surname;
+        System.out.println(id);
+    }
+}
