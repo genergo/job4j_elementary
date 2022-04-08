fcff898de495755e3c597a1fc3697e863f39b92d
genergo <genergo.ufa@gmail.com>
2022-04-08 10:22:32 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Variable/IfElseReturn.java
@@ -0,0 +1,13 @@
+package ru.job4j.Variable;
+
+public class IfElseReturn {
+    public static boolean greatThen(int first, int second) {
+        boolean compare = first > second;
+        return compare;
+        }
+
+    public static void main(String[] args) {
+        boolean result = greatThen(10, 9);
+        System.out.println(result);
+    }
+}
