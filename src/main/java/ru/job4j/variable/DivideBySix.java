ae0e4e11105a3e729729a854c057f0c08265d34e
genergo <genergo.ufa@gmail.com>
2022-04-11 11:41:35 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/variable/DivideBySix.java
+++ src/main/java/ru/job4j/variable/DivideBySix.java
@@ -3,24 +3,22 @@ package ru.job4j.variable;
 public class DivideBySix {
     public static String checkNumber(int number) {
         String rsl;
-        if (number % 3 == 0) {
-            if (number % 2 == 0) {
-                rsl = "Исходное число делится на 6.";
-            } else {
+        if (number % 3 == 0 && number % 2 == 0) {
+            rsl = "Исходное число делится на 6.";
+            } else if (number % 3 == 0) {
                 rsl = "Исходное число делится на 3, но не является четным.";
-            }
-        } else {
-            if (number % 2 == 0) {
-                rsl = "Исходное число не делится на 3, но является четным.";
+        } else if (number % 2 == 0) {
+               rsl = "Исходное число не делится на 3, но является четным.";
             } else {
                 rsl = "Исходное число не делится на 3 и не является четным.";
             }
-        }
         return rsl;
     }
 
     public static void main(String[] args) {
-
+        String rsl = DivideBySix.checkNumber(9);
+        System.out.println(rsl);
     }
 }
+
 
