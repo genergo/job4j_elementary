89410eb0c62e74e28c2c3c021f3560aab2507a4a
genergo <genergo.ufa@gmail.com>
2022-04-11 15:53:26 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/variable/SqArea.java
+++ src/main/java/ru/job4j/variable/SqArea.java
@@ -4,7 +4,7 @@ public class SqArea {
     public static double square(int p, int k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        return l * h;
+        return l + h;
         }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/variable/SqAreaTest.java
+++ src/test/java/ru/job4j/variable/SqAreaTest.java
@@ -31,4 +31,12 @@ public class SqAreaTest {
         double out = SqArea.square(p, k);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void square() {
+    }
+
+    @Test
+    public void main() {
+    }
 }
