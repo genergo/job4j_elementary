4e5388ef7423dca879f9bbf0c67e6f2b98c1b98b
genergo <genergo.ufa@gmail.com>
2022-04-11 08:53:27 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/variable/MathFunc.java
+++ src/main/java/ru/job4j/variable/MathFunc.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.variable;
 
 public class MathFunc {
 
--- src/main/java/ru/job4j/variable/Point.java
+++ src/main/java/ru/job4j/variable/Point.java
@@ -1,4 +1,4 @@
-package ru.job4j;
+package ru.job4j.variable;
 
 public class Point  {
     public static double distance(int x1, int x2, int y1, int y2) {
@@ -12,6 +12,7 @@ public class Point  {
         return rsl;
 
     }
+
     public static void main(String[] args) {
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0, 0) to (2, 0)" + result);
