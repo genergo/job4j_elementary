1ae6ac97c4083b6abe96433f3d2286719605f814
genergo <genergo.ufa@gmail.com>
2022-04-14 10:34:20 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/variable/LoopFor.java
+++ src/main/java/ru/job4j/variable/LoopFor.java
@@ -2,7 +2,7 @@ package ru.job4j.variable;
 
 public class LoopFor {
     public static void main(String[] args) {
-        for (byte index = 5; index <= 10; --index) {
+        for (byte index = 5; index <= 10; index++) {
             System.out.println(index);
         }
     }
