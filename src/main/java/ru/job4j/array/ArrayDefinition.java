7c08c91f3e411442283fdd75a2e4658fd5d57696
genergo <genergo.ufa@gmail.com>
2022-04-25 10:37:25 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        System.out.println("Размер массива равен: " + ages.length);
+        String[] surnames = new String[100500];
+        System.out.println("Размер массива равен: " + surnames.length);
+        float[] prices = new float[40];
+        System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[5];
+         names[0] = "Андрей";
+         names[1] = "Иван";
+         names[2] = "Семен";
+         names[3] = "Костя";
+        String name = names[1];
+        System.out.println(name);
+    }
+}
